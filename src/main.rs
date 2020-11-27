3fd200db9edc3b531f5d77c1a5fb1f835807c045
Robin Appelman <robin@icewind.nl>
2020-11-27 16:24:23 +0100

    add setup for doing proper authentication
    doesnt actually do any authentication yet

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use crate::connection::ActiveConnections;
 use crate::event::{Event, GroupUpdate, ShareCreate, StorageUpdate};
 use crate::storage_mapping::StorageMapping;
 pub use crate::user::UserId;
-use color_eyre::{eyre::WrapErr, Result};
+use color_eyre::{eyre::WrapErr, Report, Result};
+use futures::stream::SplitStream;
 use futures::{FutureExt, StreamExt};
 use redis::Client;
+use std::time::Duration;
 use tokio::sync::mpsc;
+use tokio::time::timeout;
+use warp::filters::ws::Message;
 use warp::ws::WebSocket;
 use warp::Filter;
 
@@ -60,31 +64,52 @@ async fn user_connected(ws: WebSocket, connections: ActiveConnections) {
         }
     }));
 
-    // Save the sender in our list of connected users.
-    let mut connection_id = None;
-    let mut user_id = None;
+    let user_id = match socket_auth(&mut user_ws_rx).await {
+        Ok(user_id) => user_id,
+        Err(e) => {
+            log::warn!("{}", e);
+            return;
+        }
+    };
 
-    // Every time the user sends a message, broadcast it to
-    // all other users...
+    let connection_id = connections.add(user_id.clone(), tx.clone());
+
+    // handle messages until the client closes the connection
     while let Some(result) = user_ws_rx.next().await {
-        let msg = match result {
+        let _msg = match result {
             Ok(msg) => msg,
             Err(e) => {
-                eprintln!("websocket error(uid={:?}): {}", connection_id, e);
+                log::warn!("websocket error: {}", e);
                 break;
             }
         };
-        if let (Ok(message), None) = (msg.to_str(), connection_id) {
-            println!("listing to changes for {}", message);
-            user_id = Some(message.into());
-            connection_id = Some(connections.add(message.into(), tx.clone()));
-        }
     }
 
-    if let (Some(connection_id), Some(user_id)) = (connection_id, user_id) {
-        // user_ws_rx stream will keep processing as long as the user stays connected
-        connections.remove(&user_id, connection_id);
+    connections.remove(&user_id, connection_id);
+}
+
+async fn read_socket_auth_message(rx: &mut SplitStream<WebSocket>) -> Result<Message> {
+    match timeout(Duration::from_secs(1), rx.next()).await {
+        Ok(Some(Ok(msg))) => Ok(msg),
+        Ok(Some(Err(e))) => Err(Report::from(e).wrap_err("Socket error during authentication")),
+        Ok(None) => Err(Report::msg("Client disconnected during authentication")),
+        Err(_) => Err(Report::msg("Authentication timeout")),
     }
+}
+
+async fn socket_auth(rx: &mut SplitStream<WebSocket>) -> Result<UserId> {
+    let username_msg = read_socket_auth_message(rx).await?;
+    let username = username_msg
+        .to_str()
+        .map_err(|_| Report::msg("Invalid authentication message"))?;
+    let password_msg = read_socket_auth_message(rx).await?;
+    let _password = password_msg
+        .to_str()
+        .map_err(|_| Report::msg("Invalid authentication message"))?;
+
+    // todo: actually authenticate
+    log::debug!("Authenticated socket for {}", username);
+    Ok(UserId::from(username))
 }
 
 async fn listen(
