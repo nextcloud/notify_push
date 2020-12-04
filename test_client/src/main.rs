952257d6cd8441589975437a1a0800a34d864bf4
Robin Appelman <robin@icewind.nl>
2020-12-04 18:10:33 +0100

    endpoint discovery for test client

--- test_client/src/main.rs
+++ test_client/src/main.rs
@@ -1,4 +1,4 @@
-use color_eyre::{eyre::WrapErr, Result};
+use color_eyre::{eyre::WrapErr, Report, Result};
 use tungstenite::{connect, Message};
 use url::Url;
 
@@ -8,15 +8,17 @@ fn main() -> Result<()> {
     let mut args = std::env::args();
 
     let bin = args.next().unwrap();
-    let (host, username, password) = match (args.next(), args.next(), args.next()) {
+    let (nc_url, username, password) = match (args.next(), args.next(), args.next()) {
         (Some(host), Some(username), Some(password)) => (host, username, password),
         _ => {
-            eprintln!("usage {} <ws url> <username> <password>", bin);
+            eprintln!("usage {} <nextcloud url> <username> <password>", bin);
             return Ok(());
         }
     };
 
-    let (mut socket, _response) = connect(Url::parse(&host).wrap_err("Invalid websocket url")?)
+    let ws_url = get_endpoint(&nc_url, &username, &password)?;
+
+    let (mut socket, _response) = connect(Url::parse(&ws_url).wrap_err("Invalid websocket url")?)
         .wrap_err("Can't connect to server")?;
 
     socket
@@ -40,4 +42,21 @@ fn main() -> Result<()> {
             println!("Received: {}", msg);
         }
     }
+}
+
+fn get_endpoint(nc_url: &str, user: &str, password: &str) -> Result<String> {
+    let json = ureq::get(&format!("{}/ocs/v2.php/cloud/capabilities", nc_url))
+        .auth(user, password)
+        .set("Accept", "application/json")
+        .set("OCS-APIREQUEST", "true")
+        .call()
+        .into_json()?;
+    Ok(
+        json["ocs"]["data"]["capabilities"]["notify_push"]["endpoints"]["websocket"]
+            .as_str()
+            .map(|url| url.to_string())
+            .ok_or(Report::msg(
+                "notify_push app not enabled or invalid capabilities response",
+            ))?,
+    )
 }
