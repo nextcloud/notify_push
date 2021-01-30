6bb73f6fa7dfb8f1c9a678b10858c69655224036
Robin Appelman <robin@icewind.nl>
2021-01-30 19:23:33 +0100

    use per-user task and channels instead of requiring a mutable connection list

--- src/connection.rs
+++ src/connection.rs
@@ -2,12 +2,12 @@ use crate::message::{DebounceMap, MessageType};
 use crate::UserId;
 use dashmap::DashMap;
 use futures::stream::SplitSink;
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 use smallvec::SmallVec;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio::task::spawn;
 use warp::ws::{Message, WebSocket};
-
-type Sender = SplitSink<WebSocket, Message>;
 
 static NEXT_CONNECTION_ID: AtomicUsize = AtomicUsize::new(1);
 
@@ -23,78 +23,104 @@ impl ConnectionId {
 
 struct UserConnection {
     id: ConnectionId,
-    sender: Sender,
+    sender: SplitSink<WebSocket, Message>,
 }
 
 #[derive(Default)]
-struct UserConnectionList {
-    debounce_map: DebounceMap,
-    connections: SmallVec<[UserConnection; 8]>,
-}
-
-#[derive(Default)]
-pub struct ActiveConnections(DashMap<UserId, UserConnectionList>);
+pub struct ActiveConnections(DashMap<UserId, UnboundedSender<UserMessage>>);
 
 pub static CONNECTION_COUNT: AtomicUsize = AtomicUsize::new(0);
 pub static MESSAGES_SEND: AtomicUsize = AtomicUsize::new(0);
 
 impl ActiveConnections {
-    pub fn add(&self, user: UserId, sender: Sender) -> ConnectionId {
+    pub fn add(&self, user: UserId, sender: SplitSink<WebSocket, Message>) -> ConnectionId {
         let id = ConnectionId::next();
-        let connection = UserConnection { id, sender };
-        self.0.entry(user).or_default().connections.push(connection);
-        CONNECTION_COUNT.fetch_add(1, Ordering::Relaxed);
+        let tx = self
+            .0
+            .entry(user)
+            .or_insert_with(|| UserTask::default().spawn());
+        let _ = tx.send(UserMessage::Add(id, sender));
         id
     }
 
     pub fn remove(&self, user: &UserId, id: ConnectionId) {
-        let should_remove = if let Some(mut user_connections) = self.0.get_mut(user) {
-            let before = user_connections.connections.len();
-            user_connections
-                .connections
-                .retain(|connection| connection.id != id);
-            let after = user_connections.connections.len();
-
-            CONNECTION_COUNT.fetch_sub(before - after, Ordering::Relaxed);
-
-            user_connections.connections.is_empty()
-        } else {
-            false
-        };
-
-        if should_remove {
-            self.0.remove(user);
+        if let Some(tx) = self.0.get(user) {
+            let _ = tx.send(UserMessage::Remove(id));
         }
     }
 
     pub async fn send_to_user(&self, user: &UserId, msg: MessageType) {
-        if let Some(mut user_connections) = self.0.get_mut(user) {
-            if user_connections.debounce_map.should_send(&msg) {
-                log::debug!(target: "notify_push::send", "Sending {} to {}", msg, user);
+        if let Some(tx) = self.0.get(user) {
+            log::debug!(target: "notify_push::send", "Sending {} to {}", msg, user);
 
-                // todo: something more clean than this (can't do retain because sending is async)
-                let mut to_cleanup = Vec::new();
+            let _ = tx.send(UserMessage::Message(msg));
+        }
+    }
+}
 
-                for connection in user_connections.connections.iter_mut() {
-                    MESSAGES_SEND.fetch_add(1, Ordering::Relaxed);
+pub enum UserMessage {
+    Add(ConnectionId, SplitSink<WebSocket, Message>),
+    Remove(ConnectionId),
+    Message(MessageType),
+}
 
-                    if let Err(e) = connection.sender.send(Message::text(msg.to_string())).await {
-                        log::info!(
-                            "Failed to send websocket message: {:#}, closing connection",
-                            e
-                        );
-                        to_cleanup.push(connection.id);
-                    }
+#[derive(Default)]
+pub struct UserTask {
+    debounce_map: DebounceMap,
+    connections: SmallVec<[UserConnection; 8]>,
+}
+
+impl UserTask {
+    fn add_connection(&mut self, id: ConnectionId, sender: SplitSink<WebSocket, Message>) {
+        let connection = UserConnection { id, sender };
+        self.connections.push(connection);
+        CONNECTION_COUNT.fetch_add(1, Ordering::Relaxed);
+    }
+
+    fn remove_connection(&mut self, id: ConnectionId) {
+        self.connections.retain(|connection| connection.id != id);
+        CONNECTION_COUNT.fetch_sub(1, Ordering::Relaxed);
+    }
+
+    async fn send(&mut self, msg: MessageType) {
+        if self.debounce_map.should_send(&msg) {
+            // todo: something more clean than this (can't do retain because sending is async)
+            let mut to_cleanup = Vec::new();
+
+            for connection in self.connections.iter_mut() {
+                MESSAGES_SEND.fetch_add(1, Ordering::Relaxed);
+
+                if let Err(e) = connection.sender.send(Message::text(msg.to_string())).await {
+                    log::info!(
+                        "Failed to send websocket message: {:#}, closing connection",
+                        e
+                    );
+                    to_cleanup.push(connection.id);
                 }
+            }
 
-                user_connections
-                    .connections
-                    .retain(|connection| !to_cleanup.contains(&connection.id));
+            self.connections
+                .retain(|connection| !to_cleanup.contains(&connection.id));
 
-                CONNECTION_COUNT.fetch_sub(to_cleanup.len(), Ordering::Relaxed);
-            } else {
-                log::trace!(target: "notify_push::send", "Not sending {} to {} due to debounce limits", msg, user);
+            CONNECTION_COUNT.fetch_sub(to_cleanup.len(), Ordering::Relaxed);
+        }
+    }
+
+    async fn run(mut self, mut rx: UnboundedReceiver<UserMessage>) {
+        while let Some(event) = rx.next().await {
+            match event {
+                UserMessage::Add(id, sender) => self.add_connection(id, sender),
+                UserMessage::Remove(id) => self.remove_connection(id),
+                UserMessage::Message(message) => self.send(message).await,
             }
         }
+    }
+
+    pub fn spawn(self) -> UnboundedSender<UserMessage> {
+        let (tx, rx) = unbounded_channel();
+
+        spawn(self.run(rx));
+
+        tx
     }
 }
