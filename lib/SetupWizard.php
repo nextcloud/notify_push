fd574230ec86128585d4944c4f62ba7e000b7498
Robin Appelman <robin@icewind.nl>
2021-02-10 19:58:17 +0100

    change reconect wrapper to only check connection after inactivity
    instead of always checking every minute, only check the connection after a minute of inactivity.
    this should better reflect how idle timeouts of sql servers work and remove most of the cases where this check was previously done.

---
+++ lib/SetupWizard.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2021 Robin Appelman <robin@icewind.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\NotifyPush;
+
+use OCP\App\IAppManager;
+
+class SetupWizard {
+	private $appManager;
+
+	public function __construct(IAppManager $appManager) {
+		$this->appManager = $appManager;
+	}
+
+	private function getBinaryPath() {
+		$basePath = realpath('../bin/');
+		$arch = php_uname('m');
+		return "$basePath/$arch/notify_push";
+	}
+
+
+}
