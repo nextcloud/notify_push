edc4c6bd33166c5bb1e8100fcac9d7bd325bb7c6
Robin Appelman <robin@icewind.nl>
2026-01-08 18:08:11 +0100

    fix: clear appconfig cache in reverse-cookie self-test
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/Controller/TestController.php
+++ lib/Controller/TestController.php
@@ -15,22 +15,26 @@ use OCP\App\IAppManager;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\DataDisplayResponse;
 use OCP\AppFramework\Http\DataResponse;
+use OCP\IAppConfig;
 use OCP\IConfig;
 use OCP\IRequest;
 
 class TestController extends Controller {
 	private $config;
+	private $appConfig;
 	private $queue;
 	private $appManager;
 
 	public function __construct(
 		IRequest $request,
 		IConfig $config,
+		IAppConfig $appConfig,
 		IQueue $queue,
 		IAppManager $appManager,
 	) {
 		parent::__construct('notify_push', $request);
 		$this->config = $config;
+		$this->appConfig = $appConfig;
 		$this->queue = $queue;
 		$this->appManager = $appManager;
 	}
@@ -41,6 +45,13 @@ class TestController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function cookie(): DataResponse {
+		// starting with 32, the app config does some internal caching
+		// that interferes with the quick set+get from this test.
+		// unfortunately the api to clear the cache is 29+ only, and we still support 27
+		if (method_exists($this->appConfig, 'clearCache')) {
+			/** @psalm-suppress UndefinedInterfaceMethod */
+			$this->appConfig->clearCache();
+		}
 		return new DataResponse((int)$this->config->getAppValue('notify_push', 'cookie', '0'));
 	}
 
